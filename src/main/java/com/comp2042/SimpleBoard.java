6971e955e5be8c5ebd8aa0c4296b2a35572c7378
Fahd-Har <fahdharis9606@gmail.com>
2025-11-01 16:56:10 +0800

    Fixing logical error, fix the swapped width and height in current game matrix to correctly represent width and height

--- src/main/java/com/comp2042/SimpleBoard.java
+++ src/main/java/com/comp2042/SimpleBoard.java
@@ -19,7 +19,7 @@ public class SimpleBoard implements Board {
     public SimpleBoard(int width, int height) {
         this.width = width;
         this.height = height;
-        currentGameMatrix = new int[width][height];
+        currentGameMatrix = new int[height][width];
         brickGenerator = new RandomBrickGenerator();
         brickRotator = new BrickRotator();
         score = new Score();
@@ -120,7 +120,7 @@ public class SimpleBoard implements Board {
 
     @Override
     public void newGame() {
-        currentGameMatrix = new int[width][height];
+        currentGameMatrix = new int[height][width];
         score.reset();
         createNewBrick();
     }
